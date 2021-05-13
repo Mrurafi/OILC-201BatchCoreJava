76401ff2319e5ec94dd9614fd31eccb41b7edc3c
R@fi25061997 <uddinrafiuddin786@gmail.com>
2021-05-13 21:04:04 +0530

    all is well

---
+++ Round3.java
@@ -0,0 +1,14 @@
+package secondinher;
+
+public class Round3 extends Roadies {
+	public String name;
+	public void intro()
+	{
+		System.out.println("interoduction");
+	}
+	public void select()
+	{
+		System.out.println("sslected");
+	}
+
+}
